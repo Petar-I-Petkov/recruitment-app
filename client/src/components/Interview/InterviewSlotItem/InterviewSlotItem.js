2bb71162e7a3bbcaf1a35779d3d47b3d3b1792d2
Petar-I-Petkov <cpt.petkov@gmail.com>
2021-09-17 06:28:45 +0300

    InterviewsAll and InterviewSlotItem  updated
    InterviewsAll styling updated.
    InterviewSlotItem styling updated.
    
    #5

--- client/src/components/Interview/InterviewSlotItem/InterviewSlotItem.js
+++ client/src/components/Interview/InterviewSlotItem/InterviewSlotItem.js
@@ -1,10 +1,29 @@
 import './InterviewSlotItem';
 
 
-const InterviewSlotItem = () => {
+import * as notificationPlugin from '../../../utils/notificationPlugin/notificationPlugin';
+import useFetch from '../../../hooks/useFetch';
+
+const InterviewSlotItem = ({
+    candidateId,
+    jobId
+}) => {
+
+    const getCandidateUrl = 'http://localhost:5000/candidates';
+    const getJobUrl = 'http://localhost:5000/jobs';
+
+    const candidateRes = useFetch(`${getCandidateUrl}/${candidateId}`,{});
+    const candidate = candidateRes.response;
 
     return (
-        <h2>Interview Slot Item</h2>
+        <article className="interview-slot-article frow j-cen w-250 h-100 border mt-20 ml-20">
+            {candidate
+                ?
+                <h2>{candidate.firstName}</h2>
+                :
+                notificationPlugin.renderLoadingBoxLocal()
+            }
+        </article>
     )
 }
 
