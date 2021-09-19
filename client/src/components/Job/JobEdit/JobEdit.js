ffc4f2e3ed2d861590c21e75783ce9a310171d77
Petar-I-Petkov <cpt.petkov@gmail.com>
2021-09-19 19:04:35 +0300

    Update JobEdit.js
    simple article added with title and description, not controlled yet
    #13

--- client/src/components/Job/JobEdit/JobEdit.js
+++ client/src/components/Job/JobEdit/JobEdit.js
@@ -22,13 +22,22 @@ const JobEdit = ({
             });
     },[]);
 
-    console.log(job);
-
-
-
-
     return (
-        <h1>Job Edit Component</h1>
+        <article className="job-edit-container fcol w-600 center mt-20 border a-cen j-between">
+            <section className="job-details-top frow a-cen j-cen">
+                <h2>{job.title}</h2>
+            </section>
+            <section className="job-details-middle-container fcol">
+                <section className="job-details-description-container fcol a-start j-start">
+                    <section className="job-details-description">
+                        <p className="">Description:</p>
+                        <p className="job-details-description-text">
+                            {job.description}
+                        </p>
+                    </section>
+                </section>
+            </section>
+        </article>
     )
 
 }
